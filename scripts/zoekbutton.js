328fcafc5ff525de299ea5a69f81f29fd3945cde
JKoenen99 <jap.koenen@hotmail.nl>
2019-06-03 13:20:27 +0200

    Filters toegevoegd en jquery functie geschreven

--- scripts/zoekbutton.js
+++ scripts/zoekbutton.js
@@ -1,54 +1,56 @@
-$(function()
-{
-	// Hide search wrap by default;
-	$(".search-wrap").hide();
+const searchbox = document.querySelector('input.search-box');
+const searchbutton = document.querySelector('button.search-button');
+const spanicon = document.querySelector('span.fa');
+const main = document.querySelector('main');
 
-	$(".toggle-search").on("click", function(e) 
-	{
-		// Prevent default link behavior
-		e.preventDefault();
-
-		// Stop propagation
-		e.stopPropagation();
-
-		// Toggle search-wrap
-		$(".search-wrap").slideToggle(500, function()
-		{
-			// Focus on the search bar
-			// When animation is complete
-			$("#search-bar").focus();	
-		});
-	});
-
-	// Close the search bar if user clicks anywhere
-	$(document).click(function(e)
-	{
-		var searchWrap = $(".search-wrap");
-		
-		if(!searchWrap.is(e.target) && searchWrap.has(e.target).length === 0)
-		{
-			searchWrap.slideUp(500);	
-		}
-	});    
+searchbutton.addEventListener('click', (e) => {
+  searchbox.classList.toggle('hidden');
+  
+  if(searchbox.classList.contains('hidden')) {
+    searchbutton.classList.remove('active');
+    spanicon.classList.replace('fa-times', 'fa-search');
+  } else {
+    searchbutton.classList.add('active');
+    spanicon.classList.replace('fa-search', 'fa-times');
+  }
 });
 
-
-
-
-var zoekknop = document.querySelector('button.zoek-button');
-var zoekbalk = document.querySelector('input.search-wrap');
-/* OUDE CODE
-var verhaalcard = document.querySelector('div.card');
-*/
-
-zoekknop.addEventListener('click', function() {
-    zoekknop.classList.toggle('active');
-    zoekbalk.classList.toggle('active');
-    console.log('CLICK zoekbutton');
+main.addEventListener('click', (e) => {
+  searchbox.classList.add('hidden');
+  searchbutton.classList.remove('active');
+  spanicon.classList.replace('fa-times', 'fa-search');
 });
 
-// $(document).click(function(e)
-//  {
-// 	zoekknop.classList.remove('active');
-// 	console.log('De zoekbalk is verborgen');
-// });
+function myFunction() {
+  // Declare variables
+  var input, filter, section, article, h2, i, txtValue;
+  input = document.querySelector('input.search-box');
+  filter = input.value.toUpperCase();
+  section = document.getElementById("mySection");
+  article = section.getElementsByTagName('article');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < article.length; i++) {
+    h2 = article[i].getElementsByTagName("h2")[0];
+    txtValue = h2.textContent || h2.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      article[i].style.display = "";
+    } else {
+      article[i].style.display = "none";
+    }
+  }
+}
+
+function accordion(section, heading, list) {
+  $(section).each(function() {
+    var that = this,
+        listHeight = $(this).find(list).height();
+
+    $(this).find(heading).click(function() {
+      $(this).toggleClass('plus minus');
+      $(that).find(list).slideToggle(250);
+    });
+  });
+};
+
+accordion('.filter-item', '.filter-item-inner-heading', '.filter-attribute-list');
